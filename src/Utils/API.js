f934299000e47788862df137785bb3b1e84f7a98
TLCroatt <trishacroatt@gmail.com>
2020-11-09 16:09:33 -0700

    updated API

--- src/Utils/API.js
+++ src/Utils/API.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-const getEmployees = () => {
-    return axios.get("https://randomuser.me/api/?results=50&nat=us");
+const API = {
+    getEmployees: function() {
+        return axios.get("https://randomuser.me/api/?results=50&nat=us");
+    }
 };
 
-export default getEmployees; 
+export default API; 
