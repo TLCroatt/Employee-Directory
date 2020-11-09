66a356cd2a5aec1b9f3ed1e298704dc8ccdcf26b
TLCroatt <trishacroatt@gmail.com>
2020-11-09 15:36:28 -0700

    add handleSort()

--- src/Components/DataArea.js
+++ src/Components/DataArea.js
@@ -8,53 +8,46 @@ import DataAreaContext from "../utils/DataAreaContext";
 
 //put in a functional component
 const DataArea = () => {
-    const [employees, setEmployeesState] = useState({
-        employees: [],
-        order: "ascend",
-        filteredEmployees: [],
-        headings: [
-            { name: "Image", width: "10%", },
-            { name: "Name", width: "20%", },
-            { name: "Phone", width: "10%", },
-            { name: "Email", width: "20%", },
-            { name: "DOB", width: "10%", }
-        ]
-    });
+    const [employees, setEmployees] = useState([{}]);
+    const [filteredEmployees, setFilteredEmployees] = useState([{}]);
+    const [sortEmployees, setSortEmployees] = useState(false);
+    const [headings, setHeadings] = useState([
+        { name: "Image", width: "10%" },
+        { name: "Name", width: "20%" },
+        { name: "Phone", width: "10%" },
+        { name: "Email", width: "20%" },
+        { name: "DOB", width: "10%" }
+    ]);
     
     useEffect(() => {
         console.log("hello")
-        loadEmployees()
+        API.getEmployees()
+            .then(res => {
+                setEmployees(res.data.results);
+                setFilteredEmployees(res.data.results)
+            }).catch(err => {console.log(err)}); 
     }, []);
 
-    function loadEmployees() {
-        API.getEmployees()
-            .then(results => 
-            setEmployeesState({
-                ...employees,
-                employees: results.data
-            })
-            ).catch(err => console.log(err)); 
         
-    }    
+        
+        
 
-    const [filteredEmployees, setFilteredEmployees] = useState([]);
-    const [sortEmployees, setSortEmployees] = useState(false);
+    
 
 return (
-    <DataAreaContext.Provider value={{ employeeState, handleInputChange, handleSort}}>
+   /* <DataAreaContext.Provider value={{ employeeState, handleInputChange, handleSort}}>
         <Nav />
 <div className="data-area">{employeeState.filteredEmployees.length > 0 ? <DataTable /> : <div></div>}</div>
     </DataAreaContext.Provider>
+)*/
+<div>
+    <DataTable />
+</div>
 )
 }
 
 //set state for sorting table (could be a boolean/toggle)
 //set state for filter(search) - this updates every time you run a search - will be set to same value as ^
 
-
-
-//useEffect moves data from API and stores it into state - this will have the API call
-
-//return DataTable which will render DataBody
 
 export default DataArea;
